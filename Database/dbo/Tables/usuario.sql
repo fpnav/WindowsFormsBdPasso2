e7a9120a0082516bd61364429779ec71cba7a643
fpnav <fpnav@yahoo.com.br>
2016-10-18 09:23:49 -0200

    ok

---
+++ Database/dbo/Tables/usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[usuario] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [nome]  NVARCHAR (50) NOT NULL,
+    [email] NVARCHAR (50) NOT NULL,
+    [senha] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
